cbfa10c32669c4e2098f3ab62c8495a9b36eb43c
omar-batrawy <obatrawy7@gmail.com>
2023-12-04 15:02:22 +0200

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,26 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    loadChildren: () =>
+      import('../app/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'aboutus',
+    loadChildren: () =>
+      import('../app/aboutus/aboutus.module').then((m) => m.AboutusModule),
+  },
+  {
+    path: '/auth/login',
+    loadChildren: () =>
+      import('../app/login/login.module').then((m) => m.LoginModule),
+  },
+  {
+    path: '/toDolist',
+    loadChildren: () =>
+      import('../app/todolist/todolist.module').then((m) => m.TodolistModule),
+  },
+  {
     path: 'appointment',
     loadChildren: () =>
       import('../app/appointment/appointment.module').then(
