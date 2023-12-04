659edcafe257f26a5733c0d01b0414c3dbf2d6cf
omar-batrawy <obatrawy7@gmail.com>
2023-12-04 15:45:22 +0200

    .

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-
+  loginForm = new FormGroup({
+    username: new FormControl(),
+    password: new FormControl(),
+  });
+  constructor() {}
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
 }
