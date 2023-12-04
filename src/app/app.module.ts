fca3cf180da154bf04546a283a74c80ec5004d13
omar-batrawy <obatrawy7@gmail.com>
2023-12-04 14:46:22 +0200

    ..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AppointmentComponent } from './appointment/appointment.component';
+import { LoginComponent } from './login/login.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { HomeComponent } from './home/home.component';
+import { TodolistComponent } from './todolist/todolist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AppointmentComponent,
+    LoginComponent,
+    AboutusComponent,
+    HomeComponent,
+    TodolistComponent
   ],
   imports: [
     BrowserModule,
