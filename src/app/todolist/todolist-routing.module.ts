2c7e8fd27adaab1bdb22c03013161d2b9bdb9981
omar-batrawy <obatrawy7@gmail.com>
2023-12-04 15:02:27 +0200

    .

--- src/app/todolist/todolist-routing.module.ts
+++ src/app/todolist/todolist-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodolistComponent } from './todolist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: TodolistComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class TodolistRoutingModule { }
+export class TodolistRoutingModule {}
