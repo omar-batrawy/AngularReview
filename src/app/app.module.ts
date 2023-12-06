65de0a0aa90246f3da1bf0a63e80f680d411dc5d
omar-batrawy <obatrawy7@gmail.com>
2023-12-06 09:51:11 +0200

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -10,6 +10,11 @@ import { AboutusComponent } from './aboutus/aboutus.component';
 import { HomeComponent } from './home/home.component';
 import { TodolistComponent } from './todolist/todolist.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CardComponent } from './Component/card/card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CdkDrag, CdkDropList } from '@angular/cdk/drag-drop';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -20,8 +25,19 @@ import { ReactiveFormsModule } from '@angular/forms';
     AboutusComponent,
     HomeComponent,
     TodolistComponent,
+    CardComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    CdkDropList,
+    CdkDrag,
+    FormsModule,
+    HttpClientModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
