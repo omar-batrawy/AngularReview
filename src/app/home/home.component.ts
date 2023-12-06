b7ff12c29007884e531a4d404dcca4359495d0e1
omar-batrawy <obatrawy7@gmail.com>
2023-12-06 11:12:06 +0200

    .

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,8 +8,45 @@ import { ApiService } from '../api.service';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  products: product[] = [];
   constructor(private apiService: ApiService) {}
+  displayStyle = 'none';
+  showCustomerExistsModal: boolean = false;
+  openPopup() {
+    this.displayStyle = 'block';
+  }
+  closePopup() {
+    this.displayStyle = 'none';
+  }
+
+  product: product[] | undefined;
+  selectedproduct: product = {
+    title: '',
+    price: 0,
+    description: '',
+    image: '',
+    category: '',
+  };
+  async addNewProduct() {
+    try {
+      await this.apiService.addNewProduct(this.selectedproduct);
+      this.closePopup();
+      this.selectedproduct = {
+        title: '',
+        price: 0,
+        description: '',
+        image: '',
+        category: '',
+      };
+      this.products = await this.apiService.getAllproducts();
+    } catch (error) {
+      console.error(error);
+      // if (error.status === 409) {
+      //   this.showCustomerExistsModal = true;
+      // }
+    }
+  }
+  products: product[] = [];
+
   async ngOnInit() {
     this.products = await this.apiService.getAllproducts();
   }
