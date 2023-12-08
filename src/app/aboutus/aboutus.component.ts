f71c1500865c2ff3fd75ac62418876496bb6dce6
omar-batrawy <obatrawy7@gmail.com>
2023-12-08 14:21:35 +0200

    .

--- src/app/aboutus/aboutus.component.ts
+++ src/app/aboutus/aboutus.component.ts
@@ -20,6 +20,23 @@ import Daylight from '@arcgis/core/widgets/Daylight.js';
 import Editor from '@arcgis/core/widgets/Editor.js';
 import MapImageLayer from '@arcgis/core/layers/MapImageLayer.js';
 import ElevationLayer from '@arcgis/core/layers/ElevationLayer.js';
+import Attachments from '@arcgis/core/widgets/Attachments.js';
+import BasemapLayerList from '@arcgis/core/widgets/BasemapLayerList.js';
+import BuildingExplorer from '@arcgis/core/widgets/BuildingExplorer.js';
+import Zoom from '@arcgis/core/widgets/Zoom.js';
+import BasemapToggle from '@arcgis/core/widgets/BasemapToggle.js';
+import Bookmarks from '@arcgis/core/widgets/Bookmarks.js';
+import Compass from '@arcgis/core/widgets/Compass.js';
+import CoordinateConversion from '@arcgis/core/widgets/CoordinateConversion.js';
+import Directions from '@arcgis/core/widgets/Directions.js';
+import ElevationProfile from '@arcgis/core/widgets/ElevationProfile.js';
+import Feature from '@arcgis/core/widgets/Feature.js';
+import FeatureTable from '@arcgis/core/widgets/FeatureTable.js';
+import Histogram from '@arcgis/core/widgets/Histogram.js';
+import FeatureTemplates from '@arcgis/core/widgets/FeatureTemplates.js';
+import Fullscreen from '@arcgis/core/widgets/Fullscreen.js';
+import { expand } from 'rxjs';
+import Locate from '@arcgis/core/widgets/Locate.js';
 @Component({
   selector: 'app-aboutus',
   templateUrl: './aboutus.component.html',
@@ -44,97 +61,212 @@ export class AboutusComponent {
       zoom: 13,
     });
 
-    const featureLayer = new FeatureLayer({
-      url: 'https://services.arcgis.com/P3ePLMYs2RVChkJx/ArcGIS/rest/services/World_Countries/FeatureServer/0',
-    });
-    view.map.add(featureLayer);
-
-    //
-    const measurement = new Measurement({
-      view: view,
-      activeTool: 'distance',
-    });
-    // view.ui.add(measurement, 'top-right');
-
-    let layerList = new LayerList({
-      view: view,
-    });
-    // Adds widget below other elements in the top left corner of the view
-    // view.ui.add(layerList, {
-    //   position: 'bottom-left',
-    // });
-
-    // let legend = new Legend({
-    //   view: view,
-    // });
-
-    // view.ui.add(legend, 'bottom-right');
-    // let basemapGallery = new BasemapGallery({
-    //   view: view,
-    // });
-    // // Add widget to the top right corner of the view
-    // view.ui.add(basemapGallery, {
-    //   position: 'top-right',
-    // });
     const searchWidget = new Search({
       view: view,
       allPlaceholder: 'search for a country',
-
-      // sources: [{}],
     });
-    // searchWidget.on('select-result', function (event) {
-    //   view.goTo({
-    //     target: event.result.feature.geometry,
-    //     zoom: 5,
-    //   });
-    // });
+
     searchWidget.on('search-complete', (event) => {
       this.searchvariable = event;
-
       console.log('search  : ', searchWidget);
       console.log(' event : ', event);
     });
 
-    // Adds the search widget below other elements in
-    // // the top left corner of the view
-    // view.ui.add(searchWidget, {
-    //   position: 'top-left',
-    //   index: 2,
-    // });
+    const featureLayer = new FeatureLayer({
+      url: 'https://services.arcgis.com/P3ePLMYs2RVChkJx/ArcGIS/rest/services/World_Countries/FeatureServer/0',
+    });
 
     const layerListExpand = new Expand({
-      expandIcon: 'layers', // see https://developers.arcgis.com/calcite-design-system/icons/
-      // expandTooltip: "Expand LayerList", // optional, defaults to "Expand" for English locale
+      expandIcon: 'layers',
       view: view,
-      content: layerList,
+      content: new LayerList({
+        view: view,
+      }),
     });
     const lmeasureexpand = new Expand({
-      expandIcon: 'measure', // see https://developers.arcgis.com/calcite-design-system/icons/
-      // expandTooltip: "Expand LayerList", // optional, defaults to "Expand" for English locale
+      expandIcon: 'measure',
       view: view,
-      content: measurement,
+      content: new Measurement({
+        view: view,
+        activeTool: 'distance',
+      }),
     });
     const SearchExpand = new Expand({
-      expandIcon: 'search', // see https://developers.arcgis.com/calcite-design-system/icons/
-      // expandTooltip: "Expand LayerList", // optional, defaults to "Expand" for English locale
+      expandIcon: 'search',
       view: view,
       content: searchWidget,
     });
-
-    view.ui.add(layerListExpand, 'bottom-left');
-    view.ui.add(lmeasureexpand, 'top-right');
-    view.ui.add(SearchExpand, 'top-left');
-    let sketch = new Sketch({
-      layer: featureLayer,
+    const Legendexpand = new Expand({
+      expandIcon: 'legend',
       view: view,
+      content: new Legend({
+        view: view,
+      }),
     });
     const SketchExpand = new Expand({
-      expandIcon: 'pencil-mark', // see https://developers.arcgis.com/calcite-design-system/icons/
-      // expandTooltip: "Expand LayerList", // optional, defaults to "Expand" for English locale
+      expandIcon: 'pencil-mark',
       view: view,
-      content: sketch,
+      content: new Sketch({
+        layer: featureLayer,
+        view: view,
+      }),
     });
-    view.ui.add(SketchExpand, 'bottom-right');
+    const compasexpand = new Expand({
+      expandIcon: 'compass',
+      view: view,
+      content: new Compass({
+        view: view,
+      }),
+    });
+
+    const basemapexpand = new Expand({
+      expandIcon: 'basemap',
+      view: view,
+      content: new BasemapGallery({
+        view: view,
+      }),
+    });
+    const basemaolistexpand = new Expand({
+      expandIcon: 'layer-basemap',
+      view: view,
+      content: new BasemapLayerList({
+        view: view,
+      }),
+    });
+    const basemaptoggleexpand = new Expand({
+      expandIcon: 'toggle-off',
+      view: view,
+      content: new BasemapToggle({
+        view: view,
+        nextBasemap: 'satellite',
+      }),
+    });
+
+    const BookmarksExpand = new Expand({
+      expandIcon: 'bookmark',
+      view: view,
+      content: new Bookmarks({
+        view: view,
+        editingEnabled: true,
+      }),
+    });
+
+    const CoordinateConversionExpand = new Expand({
+      expandIcon: 'oriented-imagery-layer',
+      view: view,
+      content: new CoordinateConversion({
+        view: view,
+      }),
+    });
+    const directionexpand = new Expand({
+      expandIcon: 'bear-left',
+      view: view,
+      content: new Directions({
+        view: view,
+      }),
+    });
+    const ElevationProfileExpand = new Expand({
+      expandIcon: 'antenna-height',
+      view: view,
+      content: new ElevationProfile({
+        view: view,
+      }),
+    });
+    const Featureexpand = new Expand({
+      expandIcon: 'feature',
+      view: view,
+      content: new Feature({
+        view: view,
+      }),
+    });
+    const zoomexpand = new Expand({
+      expandIcon: 'zoom-in-fixed',
+      view: view,
+      content: new Zoom({
+        view: view,
+      }),
+    });
+
+    let layer = new MapImageLayer({
+      url: 'https://services11.kaenergyutilities.com/server/rest/services/DoE/DOE_Plot_Vmap/MapServer',
+    });
+    const buildingexplorerexpand = new Expand({
+      expandIcon: 'line-of-sight',
+      view: view,
+      content: new BuildingExplorer({
+        layers: [layer],
+      }),
+    });
+    const featureTable = new FeatureTable({
+      view: view, // The view property must be set for the select/highlight to work
+      layer: featureLayer,
+      container: 'tableDiv',
+    });
+    const templates = new FeatureTemplates({
+      container: 'templatesDiv',
+      layers: [featureLayer],
+    });
+    const fullscreenexpand = new Expand({
+      expandIcon: 'extent',
+      view: view,
+      content: new Fullscreen({
+        view: view,
+      }),
+    });
+    const locateexpand = new Expand({
+      expandIcon: 'locate',
+      view: view,
+      content: new Locate({
+        view: view,
+      }),
+    });
+    // const params = {
+    //   layer: povLayer,
+    //   field: "POP_POVERTY",
+    //   normalizationField: "TOTPOP_CY",
+    //   numBins: 30
+    // };
+
+    // histogram(params)
+    //   .then(function(histogramResult) {
+    //      const histogram = Histogram.fromHistogramResult(histogramResult);
+    //      histogram.container = "histogram";
+    //   })
+    //    .catch(function(error) {
+    //      console.log("there was an error: ", error);
+    //    });
+
+    view.ui.add(
+      [
+        layerListExpand,
+        lmeasureexpand,
+        SearchExpand,
+        Legendexpand,
+        SketchExpand,
+        basemapexpand,
+        basemaolistexpand,
+        basemaptoggleexpand,
+        BookmarksExpand,
+        buildingexplorerexpand,
+        compasexpand,
+        CoordinateConversionExpand,
+      ],
+      'top-right'
+    );
+    view.ui.add(
+      [
+        directionexpand,
+        ElevationProfileExpand,
+        zoomexpand,
+        fullscreenexpand,
+        locateexpand,
+      ],
+      'top-left'
+    );
+    view.map.add(layer);
+    view.map.add(featureLayer);
+
+    //second map
 
     let Secondview = new SceneView({
       container: 'viewDivtwo',
@@ -166,61 +298,10 @@ export class AboutusComponent {
       }),
       group: 'top-right',
     });
+
     Secondview.ui.add([weatherExpand, daylightExpand], 'top-right');
 
-    /***********************************
-     * Add functionality to change between flooding and no flooding
-     ***********************************/
-    // Wait for the view to be loaded, in order to being able to retrieve the layer
-    //   view.when(() => {
-    //     // Find the layer for the
-    //     let floodLevel = scene.allLayers.find(function (layer) {
-    //       return layer.title === "Flood Level";
-    //     });
-
-    //     const selection = document.getElementById("selection");
-
-    //     selection.addEventListener("calciteSegmentedControlChange", () => {
-    //       switch (selection.selectedItem.value) {
-    //         case "flooding":
-    //           // Change the weather to rainy to match the flooding scenario
-    //           view.environment.weather = {
-    //             type: "rainy", // autocasts as new RainyWeather({ cloudCover: 0.7, precipitation: 0.3 })
-    //             cloudCover: 0.7,
-    //             precipitation: 0.3
-    //           };
-    //           // Turn on the water layer showing the flooding
-    //           floodLevel.visible = true;
-    //           break;
-
-    //         case "noFlooding":
-    //           // Change the weather back to cloudy
-    //           view.environment.weather = {
-    //             type: "cloudy", // autocasts as new CloudyWeather({ cloudCover: 0.3 })
-    //             cloudCover: 0.3
-    //           };
-
-    //           // Turn off the water layer showing the flooding
-    //           floodLevel.visible = false;
-    //           break;
-    //       }
-    //     })
-    //   })
-    // });
-
-    sketch.on('create', function (event) {
-      // check if the create event's state has changed to complete indicating
-      // the graphic create operation is completed.
-      if (event.state) {
-        // remove the graphic from the layer. Sketch adds
-        // the completed graphic to the layer by default.
-        // graphicsLayer.remove(event.graphic);
-        console.log('State of the event : ', event.state);
-
-        // // use the graphic.geometry to query features that intersect it
-        // selectFeatures(event.graphic.geometry);
-      }
-    });
+    //third map
 
     const webmaptwo = new WebMap({
       portalItem: {
@@ -241,10 +322,5 @@ export class AboutusComponent {
       // Add the widget to the view
       viewthree.ui.add(editor, 'top-right');
     });
-
-    let layer = new MapImageLayer({
-      url: 'https://services11.kaenergyutilities.com/server/rest/services/DoE/DOE_Plot_Vmap/MapServer',
-    });
-    view.map.add(layer); // adds the layer to the map
   }
 }
